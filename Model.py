f07f3ff71700611895cd176a3f1dbe32f2ad5267
UmeshWagharalkar <umeshwagharalkar@gmail.com>
2023-01-27 12:50:25 +0530

    Added 3 new more model code

--- Model.py
+++ Model.py
@@ -1,2 +1,11 @@
 
 print('Adding all ML related code here')
+
+def Model1():
+    print('Adding code for ML Model1')
+
+def Model2():
+    print('Adding code for ML Model2')
+
+def Model3():
+    print('Adding code for ML Model3')
